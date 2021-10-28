5f9974caf6c95a950af97c469effc492ef74ac59
NickNocc <89278623+NickNocc@users.noreply.github.com>
2021-10-27 18:38:48 -0700

    Added code preventing the user from selecting no attributes for they password

--- Develop/script.js
+++ Develop/script.js
@@ -23,65 +23,57 @@ var mods = function() {
   }
    while (go === false) { 
     var userMods = window.prompt(
-      "Type 'Lowercase', 'Uppercase', 'Numbers', or 'Special' to add or remove it from these attributs to your password. Enter 'Go' to generate your password."
+      "Type 'Lowercase', 'Uppercase', 'Numbers', or 'Special' to add or remove it from these attributes to your password. Enter 'Go' to generate your password."
     );
     if (userMods == "" || userMods == "null") {
       window.alert("Please enter a valid response");
       mods();
     }
     // Still need a way to check that they entered the right response
-    // userMods.toLowerCase;
+    userMods.toLowerCase;
     switch(userMods) {
       case "lowercase":
         if (!low) {
           low = true;
           window.alert("Your password will NOW contain lowercase letters");
-          console.log(low);
           break;
         }
         else {
           low = false;
           window.alert("Your password will NOT contain lowercase letters");
-          console.log(low);
           break;
         }
       case "uppercase":
         if (!upp) {
           upp = true;
           window.alert("Your password will NOW contain uppercase letters");
-          console.log(upp);
           break;
         }
         else {
           upp = false;
           window.alert("Your password will NOT contain uppercase letters");
-          console.log(upp);
           break;
         }
       case "numbers":
         if (!numb) {
           numb =  true;
           window.alert("Your password will NOW contain numbers");
-          console.log(numb);
           break;
         }
         else {
           numb = false;
           window.alert("Your password will NOT contain numbers");
-          console.log(numb);
           break;
         }
       case "special":
         if (!spec) {
           spec = true;
           window.alert("Your password will NOW contain special characters");
-          console.log(spec);
           break;
         }
         else {
           spec = false;
           window.alert("Your password will NOT contain special characters");
-          console.log(spec);
           break;
         }
       case "go":
@@ -98,10 +90,13 @@ var mods = function() {
         if (spec) {
           specialCharacters = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
         }
+        if (low == false && upp == false && numb == false && spec == false){
+          go = false;
+          window.alert("Please select at least one attribute");
+        }
         break;
       default:
         window.alert("You did not pick a valid option. Try again.");
-        break;
     }
    }
 
@@ -111,9 +106,8 @@ var mods = function() {
 var writePassword =function() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-  window.alert("Password Generated: " + password)
+  window.alert("Password Generated!");
 };
 
 var generatePassword = function() {
@@ -121,9 +115,6 @@ var generatePassword = function() {
   var passwordOptions = (specialCharacters + lowercaseCharacters + uppercaseCharacters + numberList);
   for (var i = 0; i <= length; i++) {
   userPassword += passwordOptions.charAt(Math.random() * passwordOptions.length);
-  console.log(userPassword);
-  console.log(length);
-  console.log(passwordOptions);
   }
   return userPassword;
 };
