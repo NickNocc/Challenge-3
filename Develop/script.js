12d6470eb99299fa5186682b3a002d471aa24dd0
NickNocc <89278623+NickNocc@users.noreply.github.com>
2021-10-27 18:28:29 -0700

    Revised existing functions to correctly generate passwords
    Also made change to allow multiple passwords to be made in the same browser

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,9 @@
 // Assignment code here
-var low = false;
-var upp = false;
-var numb = false;
-var spec = false;
-var go = false;
 var specialCharacters = "";
 var uppercaseCharacters = "";
 var lowercaseCharacters = "";
 var numberList = "";
-var userPassword = "";
-var length = 0
-
+var length = 0;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -18,6 +11,16 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 
 var mods = function() {
+  var low = false;
+  var upp = false;
+  var numb = false;
+  var spec = false;
+  var go = false;
+  length = parseInt(window.prompt("How many characters would you like in your password (8-128)?"));
+  if (length == "" || length == "null" || !length || length < 8 || length > 128) {
+    window.alert("Please enter a valid response");
+    mods();
+  }
    while (go === false) { 
     var userMods = window.prompt(
       "Type 'Lowercase', 'Uppercase', 'Numbers', or 'Special' to add or remove it from these attributs to your password. Enter 'Go' to generate your password."
@@ -83,51 +86,48 @@ var mods = function() {
         }
       case "go":
         go = true;
+        if (low) {
+          lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+        }
+        if (upp) {
+          uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        }
+        if (numb) {
+          numberList = "0123456789";
+        }
+        if (spec) {
+          specialCharacters = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+        }
         break;
       default:
         window.alert("You did not pick a valid option. Try again.");
         break;
     }
    }
+
    writePassword();
 };
 
-var generatePassword = function() {
-  var passwordOptions = (specialCharacters + lowercaseCharacters + uppercaseCharacters + numberList);
-  var length = parseInt(window.prompt("How many characters would you like in your password (8-128)?"));
-  if (low) {
-    lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
-  }
-  if (upp) {
-    uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  }
-  if (numb) {
-    numberList = "0123456789";
-  }
-  if (spec) {
-    specialCharacters = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-  }
-  console.log(passwordOptions.length);
-  console.log(passwordOptions);
-  if (length == "" || length == "null" || !length || length < 8 || length > 128) {
-    window.alert("Please enter a valid response");
-    generatePassword();
-  }
-  else {
-    for (var i = 0; i <= length; i++) {
-    userPassword += passwordOptions.charAt(Math.random() * passwordOptions.length);
-    console.log(userPassword);
-  }
-}
-};
-
-function writePassword() {
+var writePassword =function() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
   window.alert("Password Generated: " + password)
 };
+
+var generatePassword = function() {
+  var userPassword = "";
+  var passwordOptions = (specialCharacters + lowercaseCharacters + uppercaseCharacters + numberList);
+  for (var i = 0; i <= length; i++) {
+  userPassword += passwordOptions.charAt(Math.random() * passwordOptions.length);
+  console.log(userPassword);
+  console.log(length);
+  console.log(passwordOptions);
+  }
+  return userPassword;
+};
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", mods);
